a409c76833777b85c776c7d965f3fae12fc5e59b
ilhamfi <ilhamfi_2701@yahoo.com>
2017-08-22 23:20:44 +0700

    configure seed to make admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+channel_list = [
+  ["channel01", "TRANS7"],
+  ["channel02", "RCTI"],
+  ["channel03", "SCTV"],
+  ["channel04", "TRANS"],
+  ["channel05", "GTV"],
+  ["channel06", "IVM"],
+  ["channel07", "MNCTV"],
+  ["channel08", "METRO"],
+  ["channel09", "TVONE"],
+  ["channel10", "ANTV"],
+  ["channel11", "TVRI"],
+  ["channel12", "RTV"],
+  ["channel13", "INEWSTV"],
+  ["channel14", "OCHNL"],
+  ["channel15", "NET"],
+  ["channel16", "KOMPASTV"]
+]
+
+channel_list.each.with_index do |list, index|
+  id = index + 1
+  Channel.find_or_create_by(id: id, channel_id: list[0], name: list[1])
+end
+
+# creating first account for admin
+admin_exists = User.find_by_admin(true)
+unless admin_exists
+  u = User.new(email: "admin@vislog.com", username: "admin", password: "admin123", password_confirmation: "admin123", admin: true, name: "Admin")
+  u.save!
+end
