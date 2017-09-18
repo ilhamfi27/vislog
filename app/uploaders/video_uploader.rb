fa84620b052bea2d8154fd5d197dcab16433bcf3
ilhamfi <ilhamfi_2701@yahoo.com>
2017-09-19 06:11:56 +0700

    encoding video

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -12,6 +12,8 @@ class VideoUploader < CarrierWave::Uploader::Base
     include Cloudinary::CarrierWave
   end
 
+  include CarrierWave::FFMPEG
+
   # Choose what kind of storage to use for this uploader:
   storage :file if Rails.env == "development"
   # storage :fog
@@ -23,36 +25,16 @@ class VideoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.channel.channel_id.titleize}/#{model.created_at.strftime('%Y_%m_%d')}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  version :mp4 do
+    process encode_video: [:mp4]
+  end
+
+  # version :webm do
+  #   process encode_video: [:webm]
   # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
   def extension_white_list
     %w(mp4)
   end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
 
 end
