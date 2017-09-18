c15d367f1b2c39c3f7c6637aca83ddd68010931f
ilhamfi <ilhamfi_2701@yahoo.com>
2017-09-19 06:10:06 +0700

    fix chart error

--- app/models/television_program.rb
+++ app/models/television_program.rb
@@ -30,9 +30,19 @@ class TelevisionProgram < ActiveRecord::Base
   belongs_to :channel
   has_one :post_buy, dependent: :destroy 
   accepts_nested_attributes_for :post_buy
-  # validates :program, :level_1, :level_2, :cost, :channel_id, :start_time, :end_time, :duration, :st_video, :et_video, presence: true
-  # validates :cost, :start_time, :end_time, :duration, :st_video, :et_video, numericality: { only_integer: true }
-  mount_uploader :video, VideoUploader
+  validates :program, :level_1, :level_2, :cost, :channel_id, :start_time, :end_time, :duration, :st_video, :et_video, presence: true
+  validates :cost, :start_time, :end_time, :duration, :st_video, :et_video, numericality: { only_integer: true }
+  mount_uploader :video, VideoUploader do
+    def url(format = nil)
+      uploaded_path = encode_path(file.path.sub(File.expand_path(root), ''))
+      return uploaded_path if format.nil?
+      files = Dir.entries(File.dirname(file.path))
+      files.each do |f|
+        next unless File.extname(f) == '.' + format.to_s
+        return File.dirname(uploaded_path) + '/' + f
+      end
+    end
+  end
 
   class << self
     def dashboard_search(keyword, columns, date)
@@ -43,12 +53,10 @@ class TelevisionProgram < ActiveRecord::Base
           params[:channel_id_in] = c.ids 
         end
         d = date.split(' - ')
-        # params[:created_at_gteq] = d[0]
-        # params[:created_at_lteq] = d[1]
         params[:m] = 'or'
-        self.joins(:channel, post_buy: [:viewer]).between_date(d[0], d[1]).ransack(params)
+        self.join_with_channel_and_post_buy_and_viewer_table.between_date(d[0], d[1]).ransack(params)
       else
-        self.joins(:channel, post_buy: [:viewer]).ransack
+        self.join_with_channel_and_post_buy_and_viewer_table.ransack
       end
     end
   end
@@ -57,8 +65,12 @@ class TelevisionProgram < ActiveRecord::Base
     where("television_programs.created_at >= ? AND television_programs.created_at <= ?",range_1 ,range_2 )
   end
 
+  def self.join_with_channel_and_post_buy_and_viewer_table
+    joins('LEFT JOIN "channels" ON "channels"."id" = "television_programs"."channel_id" LEFT JOIN "post_buys" ON "post_buys"."television_program_id" = "television_programs"."id" LEFT JOIN "viewers" ON "viewers"."post_buy_id" = "post_buys"."id"')
+  end
+
   def self.chart_data
-    select('channels.id, channels.name AS channel_name, sum(viewers."000s") AS sum_of_rating').group('television_programs.channel_id, channels.id, channels.name, viewers."000s"')
+    select('channels.id, channels.name AS channel_name, sum(viewers."000s") AS sum_of_rating').group('channels.id, channels.name')
   end
 
   def self.chart_channel_names
@@ -71,38 +83,5 @@ class TelevisionProgram < ActiveRecord::Base
 
   def self.no_of_spot_data
     select('sum(viewers."000s") AS sum_no_of_spot')
-  end
-
-  def self.import(file)
-    spreadsheet = open_spreadsheet(file)
-    (2..spreadsheet.last_row).each do |i|
-      # code here
-      channel = Channel.find_or_initialize_by(name: spreadsheet.row(i)[1])
-      television_program = new
-      television_program.program = spreadsheet.row(i)[2]
-      television_program.level_1 = spreadsheet.row(i)[3]
-      television_program.level_2 = spreadsheet.row(i)[4]
-      television_program.cost = spreadsheet.row(i)[8].to_i
-      television_program.created_at = spreadsheet.row(i)[0]
-      television_program.updated_at = spreadsheet.row(i)[0]
-      television_program.channel_id = channel.id
-      television_program.start_time = spreadsheet.row(i)[5].split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b}
-      television_program.end_time = spreadsheet.row(i)[6].split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b}
-      television_program.duration = spreadsheet.row(i)[7].split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b}
-      television_program.st_video = spreadsheet.row(i)[9].split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b}
-      television_program.et_video = spreadsheet.row(i)[10].split(':').map { |a| a.to_i }.inject(0) { |a, b| a * 60 + b}
-      unless television_program.save && television_program.valid?
-        return false
-      end
-    end
-  end
-
-  def self.open_spreadsheet(file)
-    case File.extname(file.original_filename)
-      when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
-      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
-      when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
-      else raise "Unknown file type: #{file.original_filename}"
-    end
   end
 end
